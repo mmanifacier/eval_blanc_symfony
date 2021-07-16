36f1318d5cacfd54801a007cf086b76a7be4ae18
mmanifacier <marie.manifacier@hotmail.fr>
2021-07-16 14:45:06 +0200

    register form

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Length;
@@ -17,13 +18,21 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('username')
-            ->add('plainPassword', RepeatedType::class, [
+            ->add('username', TextType::class, [
+                'attr'=> [
+                    'class'=>'form-control',
+                    'placeholder'=>'Indiquez votre username'
+                ]
+            ])
+            ->add('password', RepeatedType::class, [
                 'type' => PasswordType::class,
-                'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+                'mapped'=>false,
+                'attr' => [
+                    'autocomplete' => 'new-password',
+                    'class'=>'form-control',
+                    'placeholder'=>'Indiquez votre password'
+                ],
                 'invalid_message' => 'The password fields must match.',
-                'options' => ['attr' => ['class' => 'password-field']],
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Please enter a password',
@@ -38,9 +47,24 @@ class RegistrationFormType extends AbstractType
                 'first_options'  => ['label' => 'Password'],
                 'second_options' => ['label' => 'Repeat Password'],
             ])
-            ->add('email', EmailType::class)
-            ->add('firstname')
-            ->add('lastname')
+            ->add('email', EmailType::class, [
+                'attr'=> [
+                    'class'=>'form-control',
+                    'placeholder'=>'Indiquez votre email'
+                ]
+            ])
+            ->add('firstname', TextType::class, [
+                'attr'=> [
+                    'class'=>'form-control',
+                    'placeholder'=>'Indiquez votre firstname'
+                ]
+            ])
+            ->add('lastname', TextType::class, [
+                'attr'=> [
+                    'class'=>'form-control',
+                    'placeholder'=>'Indiquez votre lastname'
+                ]
+            ])
         ;
     }
 
